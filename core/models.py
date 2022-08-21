d979d4137e2cae0d5bc973d0f334e24911b3d4b5
vedaantBali <vedaantbali@gmail.com>
2022-08-21 10:52:35 +0530

    Add Contact Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    phone_number = models.IntegerField(blank=True, null=True)
+    address_line_1 = models.CharField(max_length=32)
+    address_line_2 = models.CharField(max_length=32)
+    # city = models.TextChoices()
+class Dentist(models.Model):
+    name = models.CharField(max_length=32)
+
+class Company(models.Model):
+    name = models.CharField(max_length=32)
