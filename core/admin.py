14c229d19812acc06cfec1f14fdf6a83f3520feb
vedaantBali <vedaantbali@gmail.com>
2022-08-30 20:40:50 +0530

    Updated Admin

--- core/admin.py
+++ core/admin.py
@@ -1,22 +1,46 @@
 from django.contrib import admin
 from . import models
+
 # Register your models here.
 
 
-class DentistAdmin(admin.StackedInline):
-    model = models.Dentist
+class OrderAdmin(admin.TabularInline):
+    model = models.Order
+
+    readonly_fields = [
+        "dentist",
+        "items",
+        "amount",
+    ]
 
 
-class CompanyAdmin(admin.StackedInline):
+class CompanyAdmin(admin.ModelAdmin):
     model = models.Company
 
+    inlines = [
+        OrderAdmin,
+    ]
 
-class ContactAdmin(admin.ModelAdmin):
-    inlines = [DentistAdmin, CompanyAdmin]
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        current_company = request.resolver_match.kwargs["object_id"]
+
+        if db_field.name == "orders":
+            kwargs["queryset"] = models.Order.objects.filter(
+                company__id=current_company
+            )
+        if db_field.name == "products":
+            kwargs["queryset"] = models.ProductByCompany.objects.filter(
+                maker__id=current_company
+            )
+        if db_field.name == "auctions":
+            kwargs["queryset"] = models.Auction.objects.filter(
+                companies__in=[current_company]
+            )
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
 
 
 admin.site.register(models.Dentist)
-admin.site.register(models.Company)
+admin.site.register(models.Company, CompanyAdmin)
 admin.site.register(models.Product)
 admin.site.register(models.Item)
 admin.site.register(models.Inventory)
@@ -24,4 +48,4 @@ admin.site.register(models.Auction)
 admin.site.register(models.ProductByCompany)
 admin.site.register(models.AuctionHistory)
 admin.site.register(models.Order)
-admin.site.register(models.Contact, ContactAdmin)
+admin.site.register(models.Contact)
